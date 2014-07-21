808ae01d858acb4e388c66d5d6bca91a8f597a8b
tmurray <tmurray@wumpus-cave.net>
2014-07-21 16:53:39 -0500

    Swap input/output pins and set them properly

--- examples/basic_gpio.pl
+++ examples/basic_gpio.pl
@@ -5,8 +5,12 @@ use Device::PCDuino;
 
 # Take input on one pin and mirror it to the output on another
 
-use constant INPUT_PIN  => 2;
-use constant OUTPUT_PIN => 3;
+use constant INPUT_PIN  => 3;
+use constant OUTPUT_PIN => 2;
+
+
+set_input( INPUT_PIN );
+set_output( OUTPUT_PIN );
 
 while(1) {
     my $input = input( INPUT_PIN );
